4aed055fdf475f593cf4b5bc0aeb8e04d1ab873c
Xercus21 <greedyone333@gmail.com>
2025-04-21 10:56:32 +0800

    Add files via upload

---
+++ app/Models/Revenue.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Revenue extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'terminal_name',
+        'amount',
+        'date'
+    ];
+}
